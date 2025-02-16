e6bf77742ca67e33ffcd181d57b587b19409192c
abdulrahiman007 <arkaif2368@gmail.com>
2025-02-17 00:32:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -34,4 +34,6 @@ def uploaded_file(filename):
     return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Use Render's port
+    app.run(host="0.0.0.0", port=port)
+
